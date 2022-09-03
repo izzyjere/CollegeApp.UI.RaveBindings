a8ef0e356d21168d9dd5e7b0651a79321fc02897
izzyjere <wisdomjere5@gmail.com>
2022-09-04 01:53:45 +0200

    Add project files.

---
+++ CollegeApp.UI.RaveBindings/Enums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeApp.UI.RaveBindings
+{
+    public enum PaymentType
+    {
+        All,
+        MobileMoney,
+        Card,
+        BankTransfer,
+        VirtualCard
+    }
+    public enum Country
+    {
+        [Description("KE")]
+        Kenya,
+        [Description("UG")]
+        Uganda,
+        [Description("GH")]
+        Ghana,
+        [Description("NG")]
+        Nigeria,
+        [Description("NG")]
+        Rwanda,
+        [Description("NG")]
+        Zambia
+
+    }
+}
---
+++ CollegeApp.UI.RaveBindings/Options.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeApp.UI.RaveBindings
+{
+    public class Options
+    {           
+        public string PublicKey { get; set; }
+        public PaymentType PaymentType { get; set; }
+    }
+}
